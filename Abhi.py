81147005e6fac9aecc51c400ccfeca9ad04591f8
Sinchu-XD <abhiyanshicreation@gmail.com>
2025-02-23 18:21:49 +0530

    Create Abhi.py

---
+++ Abhi.py
@@ -0,0 +1,20 @@
+import telegram.ext as tg
+from aiohttp import ClientSession
+from pyrogram import Client
+from telethon import TelegramClient
+
+
+
+
+updater = tg.Updater(TOKEN, workers=WORKERS, use_context=True)
+telethn = TelegramClient("Abhi", API_ID, API_HASH)
+
+Bot = Client("CoupleBot", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN,in_memory=True)
+dispatcher = updater.dispatcher
+aiohttpsession = ClientSession()
+
+
+print("[INFO]: Getting Bot Info...")
+BOT_ID = dispatcher.bot.id
+BOT_NAME = dispatcher.bot.first_name
+BOT_USERNAME = dispatcher.bot.username
