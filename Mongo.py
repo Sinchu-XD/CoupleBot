2eea5c5fd12aaf9f95d0b10cedfa9347ec9a2dda
Sinchu-XD <abhiyanshicreation@gmail.com>
2025-02-23 18:51:44 +0530

    Update Mongo.py

--- Mongo.py
+++ Mongo.py
@@ -2,7 +2,7 @@ from typing import Dict, Union
 
 from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
 
-from Abhi import MONGO_DB_URI
+from Config import MONGO_DB_URI
 
 mongo = MongoCli(MONGO_DB_URI)
 db = mongo.Abhi
