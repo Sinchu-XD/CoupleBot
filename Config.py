31046d243ee0c557c65325aca8f2ba063c24f28f
Sinchu-XD <abhiyanshicreation@gmail.com>
2025-02-23 19:46:06 +0530

    Create Config.py

---
+++ Config.py
@@ -0,0 +1,7 @@
+import os
+
+
+API_ID = int(os.environ.get("API_ID", None))
+API_HASH = os.environ.get("API_HASH", None)
+TOKEN = os.environ.get("TOKEN", None)
+MONGO_DB_URI = os.environ.get("MONGO_DB_URI", None)
